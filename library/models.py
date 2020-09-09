0e997b00d6677a1b3b0584238a17581ce3f36e99
jawish-a@outlook.com <jawish-a@outlook.com>
2020-09-09 13:31:51 +0300

    added Membership model

--- library/models.py
+++ library/models.py
@@ -2,8 +2,8 @@ from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
 
-class Library(models.ModelForm):
-    manager = models.ForeignKey(User, default=1, on_delete=models.CASCADE)
+class Library(models.Model):
+    manager = models.ForeignKey(User, default=1, on_delete=models.CASCADE, related_name="managers")
     name = models.CharField(max_length=120)
     description = models.TextField()
     opening_time = models.TimeField()
@@ -11,3 +11,21 @@ class Library(models.ModelForm):
     # for the admin panel
     def __str__(self):
         return self.name
+class Membership(models.Model):
+    periods = (
+		("M", "Monthly"),
+		("Y", "Yearly"),
+	)
+    name = models.CharField(max_length=120)
+    period = models.CharField(max_length=1, choices=periods)
+    is_active = models.BooleanField(default=False)
+
+class Member(models.Model):
+    genders = (
+        ("M", "Male"),
+        ("F", "Female"),
+    )
+    gender = models.CharField(max_length=1, choices=genders)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    library = models.ForeignKey(Library, on_delete=models.CASCADE)
+    membership = models.OneToOneField(Membership, default=1 ,on_delete=models.CASCADE, related_name="mempership")
